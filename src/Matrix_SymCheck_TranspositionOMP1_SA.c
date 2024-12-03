790495896dd1ea39adc729fca471a94093b1a46a
federico-peruzzo <federico.peruzzo@studenti.unitn.it>
2024-12-03 15:10:43 +0100

    Add DCT (Differential Comparison Test)
    A DCT is a type of test that passes when a program/function/etc returns
    the same results of the reference one, otherwise it will fail.
    Added a macro that override the DEBUG macro definition, and disables
    the possibility to output results.
    The only output on stdout will be if the current implementation gives
    the same results of the golden one selected.

--- src/Matrix_SymCheck_TranspositionOMP1_SA.c
+++ src/Matrix_SymCheck_TranspositionOMP1_SA.c
@@ -11,6 +11,10 @@
 #ifndef RESULT_CSV_PATH
     #define RESULT_CSV_PATH "./results.csv"
 #endif
+#ifdef DIFF_COMP_TEST
+    #undef DEBUG
+    #define DEBUG 0
+#endif
 
 // ------------------------------- INCLUDES ------------------------------
 
@@ -35,6 +39,7 @@
 
 #define MAT_TRANSPOSE_FUNC matTransposeOMP1SA
 #define MAT_CHECKSYM_FUNC checkSymOMP1SA
+#define MAT_COMP_CHECKSYM_FUNC checkSymSA
 #define MAT_COMP_TRANSPOSE_FUNC matTransposeSA
 
 // --------------------------------- MAIN --------------------------------
@@ -42,6 +47,10 @@
 int main() {
     uint32_t rand_seed = time(NULL);
     srand(rand_seed);
+    #ifdef DIFF_COMP_TEST
+        char *filename = filename_from_path(__FILE__);
+    #endif
+
     MAT_TYPE M = MAT_INIT_FUNC(SIZE);
     #if DEBUG >= 1
         printf("The selected matrix side size is: %lu\n", SIZE);
@@ -54,20 +63,24 @@ int main() {
     double wt1, wt2, symCheckTime, matTransposeTime;
 
     bool isSym = MAT_CHECKSYM_FUNC(M, SIZE, &wt1, &wt2);
+    symCheckTime = wt2 - wt1;
     #if DEBUG >= 1
         printf("Is matrix Symmetric? -> %s\n", (isSym?"True":"False"));
     #endif
-    symCheckTime = wt2 - wt1;
+    #ifdef DIFF_COMP_TEST
+        bool isSymCheck = MAT_COMP_CHECKSYM_FUNC(M, SIZE, &wt1, &wt2);
+        printf("[%s] - Differential Comparison Test - Check Symmetry : %s\n", filename, ((isSym == isSymCheck)?"PASS":"NO_PASS"));
+    #endif
 
     MAT_TYPE T = MAT_TRANSPOSE_FUNC(M, SIZE, &wt1, &wt2);
+    matTransposeTime = wt2 - wt1;
     #if DEBUG >= 2
         printf("Matrix | Transposed matrix:\n");
         MAT_PRINT_FUNC(M, SIZE);
         printf("\n");
         MAT_PRINT_FUNC(T, SIZE);
     #endif
-    matTransposeTime = wt2 - wt1;
-    #if DEBUG >= 1
+    #if DEBUG >= 1 || defined(DIFF_COMP_TEST)
         MAT_TYPE TCheck = MAT_COMP_TRANSPOSE_FUNC(M, SIZE, &wt1, &wt2);
     #endif
     #if DEBUG >= 2
@@ -80,18 +93,21 @@ int main() {
     //destination of the output matrix, except for the diagonals values
     //(those are read and written twice the times respect to the others memory locations).
 
-    //Append to RESULT_CSV_PATH the results of the run
-    #if DEBUG >= 0
+    #ifdef DIFF_COMP_TEST
+        printf("[%s] - Differential Comparison Test - Transposition : %s\n", filename, (MAT_CHECKEQ_FUNC(T, TCheck, SIZE)?"PASS":"NO_PASS"));
+        free(filename);
+    #elif DEBUG >= 0
+        //Append to RESULT_CSV_PATH the results of the run
         saveResults(RESULT_CSV_PATH, rand_seed, SIZE, memory_rw, omp_get_max_threads(), symCheckTime, matTransposeTime);
     #endif
     #if DEBUG >= 1
-        printf("The two matrices are equal? -> %s\n", (MAT_CHECKEQ_FUNC(T, TCheck, SIZE)?"True":"False"));
+        printf("The two matrices(T and TCheck) are equal? -> %s\n", (MAT_CHECKEQ_FUNC(T, TCheck, SIZE)?"True":"False"));
         printf("Time to check the matrix symmetry (omp_get_wtime) = %15.9f sec\n", symCheckTime);
         printf("Time to transpose the matrix (omp_get_wtime)      = %15.9f sec\n", matTransposeTime);
     #endif
     MAT_FREE_FUNC(T);
     MAT_FREE_FUNC(M);
-    #if DEBUG >= 1
+    #if DEBUG >= 1 || defined(DIFF_COMP_TEST)
         MAT_FREE_FUNC(TCheck);
     #endif
     return 0;
